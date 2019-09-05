b5b15a5d554127f12d4af3586bc813eb763a233e
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2019-09-05 00:15:42 -0300

    Add support for handlebars

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
 
 const { User } = require('./app/models');
 
@@ -10,6 +11,9 @@ const port = process.argv[2];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use('/static', express.static(`${__dirname}/static`));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
