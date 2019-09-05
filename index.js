1e26f727c08b11a2f3b7f2fadc1ef7a78f7952e7
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2019-09-05 00:41:58 -0300

    Adiciona as views e as rotas raiz (/) e /cadastrar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const { User } = require('./app/models');
 
@@ -13,10 +14,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.use('/static', express.static(`${__dirname}/static`));
+app.set('views', path.join(__dirname, 'app/views'));
+app.use(express.static(path.join(`${__dirname}app/static`)));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('login', { titleComplement: 'Entre ou cadastre-se' });
 });
+
+app.get('/cadastrar', (req, res) => {
+  res.render('register', { titleComplement: 'Cadastar' });
+});
+
+app.post('/cadastrar', (req, res) => {});
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
